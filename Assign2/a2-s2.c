1901eb9b77373e607738bf582d7b7075cd803eb6
darshanpb07 <126590162+darshanpb07@users.noreply.github.com>
2024-02-25 23:52:49 +0530

    Update a2-s2.c

--- Assign2/a2-s2.c
+++ Assign2/a2-s2.c
@@ -1,119 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
 
-struct file {
-    char name[20];
-    int start_block;
-    int length;
-    struct file* next_block;
+// Node structure for linked list allocation
+struct Node {
+    int block_number;
+    struct Node* next;
 };
 
+// Disk structure
+struct Disk {
+    int num_blocks;
+    char** blocks; // For storing file names or NULL if block is free
+    struct Node** linked_blocks; // For storing linked list of blocks allocated to a file
+};
+
+// Function to initialize disk
+struct Disk* initializeDisk(int num_blocks) {
+    struct Disk* disk = (struct Disk*)malloc(sizeof(struct Disk));
+    disk->num_blocks = num_blocks;
+
+    disk->blocks = (char**)malloc(num_blocks * sizeof(char*));
+    disk->linked_blocks = (struct Node**)malloc(num_blocks * sizeof(struct Node*));
+
+    // Initialize all blocks as free initially
+    for (int i = 0; i < num_blocks; ++i) {
+        disk->blocks[i] = NULL;
+        disk->linked_blocks[i] = NULL;
+    }
+
+    return disk;
+}
+
+// Function to allocate blocks to a file
+void allocateFile(struct Disk* disk, char* file_name, int num_blocks) {
+    int start_block = -1;
+    for (int i = 0; i < disk->num_blocks; ++i) {
+        if (disk->blocks[i] == NULL) {
+            start_block = i;
+            break;
+        }
+    }
+
+    if (start_block == -1 || start_block + num_blocks > disk->num_blocks) {
+        printf("Error: Not enough space available to allocate file.\n");
+        return;
+    }
+
+    struct Node* current_block = NULL;
+    for (int i = 0; i < num_blocks; ++i) {
+        disk->blocks[start_block + i] = file_name;
+        struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+        new_node->block_number = start_block + i;
+        new_node->next = NULL;
+
+        if (current_block == NULL) {
+            disk->linked_blocks[start_block] = new_node;
+            current_block = new_node;
+        } else {
+            current_block->next = new_node;
+            current_block = new_node;
+        }
+    }
+
+    printf("File '%s' allocated starting from block %d.\n", file_name, start_block);
+}
+
+// Function to free blocks allocated to a file
+void freeBlocks(struct Disk* disk, int start_block) {
+    struct Node* current_block = disk->linked_blocks[start_block];
+    while (current_block != NULL) {
+        int block_number = current_block->block_number;
+        disk->blocks[block_number] = NULL;
+        struct Node* temp = current_block;
+        current_block = current_block->next;
+        free(temp);
+    }
+    disk->linked_blocks[start_block] = NULL;
+    printf("Blocks allocated to the file starting from block %d freed.\n", start_block);
+}
+
+// Function to display disk status
+void displayDiskStatus(struct Disk* disk) {
+    printf("Disk Status:\n");
+    for (int i = 0; i < disk->num_blocks; ++i) {
+        if (disk->blocks[i] == NULL) {
+            printf("Block %d: Free\n", i);
+        } else {
+            printf("Block %d: Allocated to '%s'\n", i, disk->blocks[i]);
+        }
+    }
+}
+
+// Main function
 int main() {
-    int n, num_files, i, start_block, length;
-    char file_name[20];
+    int num_blocks;
+    printf("Enter the number of blocks on the disk: ");
+    scanf("%d", &num_blocks);
 
-    printf("Enter the number of blocks in the disk: ");
-    scanf("%d", &n);
+    struct Disk* disk = initializeDisk(num_blocks);
 
-    struct file* files = NULL;
-
-    while (1) {
-        printf("\n1. Create a new file\n");
-        printf("2. Delete a file\n");
+    int choice;
+    while (true) {
+        printf("\nMenu:\n");
+        printf("1. Allocate file\n");
+        printf("2. Free file\n");
         printf("3. Display disk status\n");
         printf("4. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &i);
+        scanf("%d", &choice);
 
-        switch (i) {
-            case 1:
-                printf("\nEnter the name of the file: ");
+        switch (choice) {
+            case 1: {
+                char file_name[50];
+                int num_blocks;
+                printf("Enter the name of the file: ");
                 scanf("%s", file_name);
-                printf("Enter the starting block number: ");
+                printf("Enter the number of blocks needed for the file: ");
+                scanf("%d", &num_blocks);
+                allocateFile(disk, file_name, num_blocks);
+                break;
+            }
+            case 2: {
+                int start_block;
+                printf("Enter the starting block of the file to free: ");
                 scanf("%d", &start_block);
-                printf("Enter the length of the file: ");
-                scanf("%d", &length);
-
-                if (start_block + length > n) {
-                    printf("File allocation failed: not enough space\n");
-                } else {
-                    struct file* new_file = (struct file*) malloc(sizeof(struct file));
-                    new_file->next_block = NULL;
-                    strcpy(new_file->name, file_name);
-                    new_file->start_block = start_block;
-                    new_file->length = length;
-
-                    if (files == NULL) {
-                        files = new_file;
-                    } else {
-                        struct file* ptr = files;
-
-                        while (ptr->next_block != NULL) {
-                            ptr = ptr->next_block;
-                        }
-
-                        ptr->next_block = new_file;
-                    }
-
-                    printf("File %s created successfully\n", file_name);
-                }
-
+                freeBlocks(disk, start_block);
                 break;
-            case 2:
-                printf("\nEnter the name of the file: ");
-                scanf("%s", file_name);
-
-                struct file* ptr = files;
-                struct file* prev_ptr = NULL;
-
-                while (ptr != NULL && strcmp(ptr->name, file_name) != 0) {
-                    prev_ptr = ptr;
-                    ptr = ptr->next_block;
-                }
-
-                if (ptr == NULL) {
-                    printf("File deletion failed: file not found\n");
-                } else {
-                    if (prev_ptr == NULL) {
-                        files = ptr->next_block;
-                    } else {
-                        prev_ptr->next_block = ptr->next_block;
-                    }
-
-                    free(ptr);
-                    printf("File %s deleted successfully\n", file_name);
-                }
-
+            }
+            case 3: {
+                displayDiskStatus(disk);
                 break;
-            case 3:
-                printf("\nDisk status:\n");
-
-                int* blocks = (int*) calloc(n, sizeof(int));
-                ptr = files;
-
-                while (ptr != NULL) {
-                    for (i = ptr->start_block; i < ptr->start_block + ptr->length; i++) {
-                        blocks[i] = 1;
-                    }
-
-                    ptr = ptr->next_block;
-                }
-
-                for (i = 0; i < n; i++) {
-                    if (blocks[i] == 0) {
-                        printf("Block %d: Free\n", i);
-                    } else {
-                        printf("Block %d: Allocated\n", i);
-                    }
-                }
-
-                free(blocks);
-                break;
-            case 4:
+            }
+            case 4: {
+                printf("Exiting...\n");
                 return 0;
+            }
             default:
-                printf("Invalid choice\n");
+                printf("Invalid choice. Please try again.\n");
         }
     }
+
+    return 0;
 }
